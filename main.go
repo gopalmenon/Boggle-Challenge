f01154e47abd52b208744011169c6d6a417700d4
Gopal Menon <gopal.mnn@gmail.com>
2019-02-02 09:51:25 -0700

    Added function to process flags

--- main.go
+++ main.go
@@ -1,17 +1,25 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"math/rand"
 )
 
 const (
-	boardSide = 4
-	dieFaces  = 6
+	boardSide      = 4
+	dieFaces       = 6
+	dictionaryFile = "http://coursera.cs.princeton.edu/algs4/testing/boggle/dictionary-yawl.txt"
 )
 
-//Dice in the Boggle game
-var dice = []string{"aaeegn", "aoottw", "distty", "eiosst", "abbjoo", "cimotu", "eeghnw", "elrtty", "achops", "deilrx", "eeinsu", "himnqu", "affkps", "delrvy", "ehrtvw", "hlnnrz"}
+//Dice in the Boggle game. They are all in upper case and dictionary will also be in upper case. The letter U will be
+//implicit after Q
+var dice = []string{"AAEEGN", "AOOTTW", "DISTTY", "EIOSST", "ABBJOO", "CIMOTU", "EEGHNW", "ELRTTY", "ACHOPS", "DEILRX", "EEINSU", "HIMNQU", "AFFKPS", "DELRVY", "EHRTVW", "HLNNRZ"}
+
+//Command line flags for simulated annealing parameters
+var acceptWorse bool
+var perturbationCount, iterations int
+var initialTemp, coolingRate float64
 
 //Die number and face number of a die on the board
 type boardDie struct {
@@ -56,9 +64,40 @@ func boggleBoard() [][]boardDie {
 	return board
 }
 
+//Print the boggle board to standard output
+func printBoard(board [][]boardDie) {
+
+	//For every row on the board
+	for _, row := range board {
+		//For every die in the row
+		for _, die := range row {
+			fmt.Printf("%c ", dice[die.dieNumber][die.dieFace])
+		}
+		fmt.Printf("\n")
+	}
+
+}
+
+//Parse command line flags for whether to accept boards with worse scores for simulated annealing and if so,
+//the number of perturbations, initial temperature and temperature cooling factor
+func init() {
+
+	flag.BoolVar(&acceptWorse, "a", true, "Accept perturbed board with worse score")
+	flag.IntVar(&perturbationCount, "p", 1, "Number of dice to perturb to get next board")
+	flag.Float64Var(&initialTemp, "t", 1000, "Initial temperature")
+	flag.Float64Var(&coolingRate, "c", 0.9, "Cooling rate")
+	flag.IntVar(&iterations, "i", 1000, "Number of iterations")
+
+}
+
+//Load dictionary into prefix tree
+func loadDictionary() {
+
+}
+
 func main() {
 
 	board := boggleBoard()
+	printBoard(board)
 
-	fmt.Println(board)
 }
